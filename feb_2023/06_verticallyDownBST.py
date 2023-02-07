851868b42845a64d3c331f40cfb10400f096e29d
gaurav-batheja <123798033+gaurav-batheja@users.noreply.github.com>
2023-02-07 17:21:08 +0530

    Update 06_verticallyDownBST.py

--- feb_2023/06_verticallyDownBST.py
+++ feb_2023/06_verticallyDownBST.py
@@ -1,15 +1,18 @@
-        def dfs(self, root, targetValue, hasFound=False, targetPos=0, currPos=0):
-            if root is None:
-                return 0
-            ans = 0
-            if hasFound and targetPos == currPos:
-                ans += root.data
-            if not hasFound and root.data == targetValue:
-                hasFound = True
-                targetPos = currPos
-                ans += 1
-            ans += self.dfs(root.left, targetValue, hasFound, targetPos, currPos - 1)
-            ans += self.dfs(root.right, targetValue, hasFound, targetPos, currPos + 1)
-            return ans
-        def verticallyDownBST(self, root, target):
-            return self.dfs(root, target) - 1
+# Given a Binary Search Tree with unique node values and a target value. 
+# You have to find the node whose data is equal to target and return the sum of all descendant (of target) node's data which are vertically below the target node. 
+# Initially, you are at the root node. Note: If target node is not present in bst then return -1
+def dfs(self, root, targetValue, hasFound=False, targetPos=0, currPos=0):
+    if root is None:
+        return 0
+    ans = 0
+    if hasFound and targetPos == currPos:
+        ans += root.data
+    if not hasFound and root.data == targetValue:
+        hasFound = True
+        targetPos = currPos
+        ans += 1
+    ans += self.dfs(root.left, targetValue, hasFound, targetPos, currPos - 1)
+    ans += self.dfs(root.right, targetValue, hasFound, targetPos, currPos + 1)
+    return ans
+def verticallyDownBST(self, root, target):
+    return self.dfs(root, target) - 1
