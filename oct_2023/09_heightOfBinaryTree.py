2a9bfede71e69c23c56a5fa48303b700d6fdc59e
gaurav-batheja <123798033+gaurav-batheja@users.noreply.github.com>
2023-10-09 18:13:27 +0530

    Create 09_heightOfBinaryTree.py

---
+++ oct_2023/09_heightOfBinaryTree.py
@@ -0,0 +1,19 @@
+#Given a binary tree.Find its height
+
+#User function Template for python3
+
+'''
+# Node Class:
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+        '''
+class Solution:
+    #Function to find the height of a binary tree.
+    def height(self, root):
+        # code here
+        if not root:
+            return 0
+        return 1+max(self.height(root.left),(self.height(root.right)))
